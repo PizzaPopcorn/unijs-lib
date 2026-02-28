01b207733262359acacf876f76e4b0a92a5b72f4
Andres Urrutia <urrutiac.andres@gmail.com>
2026-02-28 12:11:04 -0600

    Prepared for cdn hosted in npm with minified version

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,20 @@
-﻿import pkg from './package.json' with { type: 'json' };
+﻿import terser from '@rollup/plugin-terser';
 
 export default {
     input: 'src/Unity.js',
-    output: {
-        name: 'Unity',
-        file: `dist/pizzapopcorn-unityjs-${pkg.version}.js`,
-        format: 'umd',
-        exports: 'default'
-    }
+    output: [
+        {
+            name: 'Unity',
+            file: `dist/unityjs.js`,
+            format: 'umd',
+            exports: 'default'
+        },
+        {
+            name: 'Unity',
+            file: `dist/unityjs.min.js`,
+            format: 'umd',
+            exports: 'default',
+            plugins: [terser()]
+        }
+    ]
 }
